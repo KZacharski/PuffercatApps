a004a6ad7222d62c55fa29f8e7fccdd66d1798c0
KZacharski <puffercatt@gmail.com>
2026-06-23 12:45:19 +0200

    preparation for twa

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,9 @@ module.exports = {
       msTileColor: '#001122',
       appleMobileWebAppCapable: 'yes',
       appleMobileWebAppStatusBarStyle: 'default',
+      manifestOptions: {
+        "description": "The full catalog of apps created by Puffercat as well as ones currently in development. You can download all of my Android apps here, from the official source."
+      }
   
       // configure the workbox plugin
     /*  workboxPluginMode: 'InjectManifest',
